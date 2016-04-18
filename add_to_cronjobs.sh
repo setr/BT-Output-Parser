1fc49822da72e57c87cc4bba1b3a301710b7210e
nokhandi@hawk.iit.edu <nokhandi@hawk.iit.edu>
2016-04-17 19:27:23 -0500

    added a starting-curl to add_to_cronjobs

--- add_to_cronjobs.sh
+++ add_to_cronjobs.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# starts an initial curl stream
+curl "https://api.particle.io/v1/devices/events?access_token=9b3de52ac7981f0af0fa0972b1a6a130ba747aa8" >> $DIR/tmp.txt &
+
 echo "* 12 * * * root $DIR/wrapper.sh " >> /etc/crontab 
